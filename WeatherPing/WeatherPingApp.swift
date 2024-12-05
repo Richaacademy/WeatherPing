45bec5193de2d8d62d431c0e54dfdcc1353809d0
Richa Mulchandani <richmulchandani30@gmail.com>
2024-12-05 15:39:09 +0100

    Initial Commit

---
+++ WeatherPing/WeatherPingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPingApp.swift
+//  WeatherPing
+//
+//  Created by Richa Mulchandani on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
